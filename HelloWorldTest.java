3cf2803230310ffe8d35c9177a4bd1e15670462e
naveenkumarchinde <71186580+naveenkumarchinde@users.noreply.github.com>
2020-09-13 22:55:03 +0530

    Add files via upload

---
+++ HelloWorldTest.java
@@ -0,0 +1,15 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
